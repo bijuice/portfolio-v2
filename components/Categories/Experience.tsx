9c247d575ac819346f3aa4a8eabb85cb42c07536
bijuice <abdul.r.zaid@gmail.com>
2023-04-09 14:28:33 +0300

    Added experiences

--- components/Categories/Experience.tsx
+++ components/Categories/Experience.tsx
@@ -1,6 +1,67 @@
 import { CategoryProps } from "@/props/CategoryProps"
 
 export default function Experience({}: CategoryProps) {
+  return (
+    <div className="h-full select-none">
+      <div className="h-full  flex items-center   relative pt-[35vh]">
+        <Timeline />
+      </div>
+    </div>
+  )
+}
+
+type Experience = {
+  year: number
+  //in months
+  duration: number
+  title: string
+  role: string
+  resps: string[]
+  type:
+    | "Education"
+    | "Volunteer"
+    | "Full Time Employment"
+    | "Freelance"
+    | "Award"
+}
+
+const experiences: Experience[] = [
+  {
+    year: 2017,
+    duration: 48,
+    title: "United States International University - Africa",
+    role: "Student",
+    type: "Education",
+    resps: [
+      "Bachelor of science in Applied Computer Technology",
+      "Concentration in Forensics and Cyber Security",
+    ],
+  },
+  {
+    year: 2020,
+    duration: 12,
+    title: "KamiLimu",
+    role: "Student",
+    type: "Education",
+    resps: [
+      "Bachelor of science in Applied Computer Technology",
+      "Concentration in Forensics and Cyber Security",
+    ],
+  },
+  {
+    year: 2020,
+    duration: 12,
+    title: "Culture Capture",
+    role: "Co-founder",
+    type: "Freelance",
+    resps: [
+      "Bachelor of science in Applied Computer Technology",
+      "Concentration in Forensics and Cyber Security",
+    ],
+  },
+]
+
+function Timeline() {
   const date = new Date()
   const year = date.getFullYear()
 
@@ -11,31 +72,31 @@ export default function Experience({}: CategoryProps) {
     let dots: React.ReactNode[] = []
 
     for (let i = a; i < b; i++) {
-      const isEven = i % 2 === 0
-
-      dots.push(
-        <div className="w-4 h-4 rounded-full border-[1px] border-secondary  relative dot">
-          <div className="w-[1.5px] h-5 bg-secondary rounded-md absolute -top-8 left-[45%]"></div>
-          <p className="text-secondary absolute top-5 -left-2">{i}</p>
-        </div>
-      )
+      dots.push(<Point key={i} year={i} />)
     }
 
     return dots
   }
 
   return (
-    <div className="h-full select-none">
-      <div className="h-full  flex items-center   relative pt-44">
-        <Timeline />
-        <div className="flex justify-between w-full absolute  py-5  ">
-          {renderDots()}
-        </div>
+    <>
+      <Line />
+      <div className="flex justify-between w-full absolute  py-5  ">
+        {renderDots()}
       </div>
+    </>
+  )
+}
+
+function Point({ year }: { year: number }) {
+  return (
+    <div className="w-4 h-4 rounded-full border-2 border-secondary  relative dot">
+      <div className="w-[2px] h-6 bg-secondary rounded-md absolute bottom-5 left-[45%]"></div>
+      <p className="text-secondary absolute -bottom-8 -left-[75%]">{year}</p>
     </div>
   )
 }
 
-function Timeline() {
+function Line() {
   return <div className="bg-secondary w-full h-[1px]  timeline relative"></div>
 }
