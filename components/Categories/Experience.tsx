d566db255410ba9f141fff0f603b10fcbc217162
bijuice <abdul.r.zaid@gmail.com>
2023-04-15 22:33:42 +0300

    minor ui fixes

--- components/Categories/Experience.tsx
+++ components/Categories/Experience.tsx
@@ -3,7 +3,7 @@ import { ReactNode, useEffect, useMemo, useState } from "react"
 
 export default function Experience({}: CategoryProps) {
   return (
-    <div className="h-full select-none">
+    <div className="h-full select-none relative">
       <div className="h-full  flex items-center   relative pt-[35vh]">
         <Timeline />
       </div>
@@ -73,9 +73,9 @@ const experiences: Experience[] = [
   },
   {
     year: 2022,
-    duration: 14,
-    title: "Pesapal",
-    role: "Developer",
+    duration: 3,
+    title: "Africa Law Partners",
+    role: "Freelance",
     resps: [
       "Bachelor of science in Applied Computer Technology",
       "Concentration in Forensics and Cyber Security",
@@ -83,9 +83,9 @@ const experiences: Experience[] = [
   },
   {
     year: 2022,
-    duration: 3,
-    title: "Africa Law Partners",
-    role: "Freelance",
+    duration: 14,
+    title: "Pesapal",
+    role: "Developer",
     resps: [
       "Bachelor of science in Applied Computer Technology",
       "Concentration in Forensics and Cyber Security",
@@ -107,6 +107,7 @@ function Timeline() {
 
   const a = year - 6
   const b = year + 2
+  const [activeExp, setActiveExp] = useState<Experience | null>(null)
 
   function renderDots() {
     let dots: React.ReactNode[] = []
@@ -115,7 +116,15 @@ function Timeline() {
       const exps = experiences.filter((e) => e.year === i)
 
       const cards = exps.map((e) => {
-        return <ExpCard key={e.title} exp={e} timelineWidth={timelineWidth} />
+        return (
+          <ExpCard
+            key={e.title}
+            exp={e}
+            timelineWidth={timelineWidth}
+            activeExp={activeExp}
+            setActiveExp={setActiveExp}
+          />
+        )
       })
 
       dots.push(<Point key={i} year={i} cards={cards} />)
@@ -157,9 +166,16 @@ function Point({ year, cards }: PointProps) {
 type ExpCardProps = {
   exp: Experience
   timelineWidth: number
+  activeExp: Experience | null
+  setActiveExp: React.Dispatch<React.SetStateAction<Experience | null>>
 }
 
-function ExpCard({ exp, timelineWidth }: ExpCardProps) {
+function ExpCard({
+  activeExp,
+  setActiveExp,
+  exp,
+  timelineWidth,
+}: ExpCardProps) {
   function monthsToYears() {
     const dur = (exp.duration / 12).toFixed(1)
 
@@ -170,10 +186,28 @@ function ExpCard({ exp, timelineWidth }: ExpCardProps) {
 
   const durationWidth = (exp.duration / 84) * timelineWidth
 
+  const cardState = () => {
+    if (activeExp === null) {
+      return "initial"
+    }
+    if (exp.title === activeExp?.title) {
+      return "active"
+    }
+
+    return "incative"
+  }
+
   return (
-    <div className="flex cursor-pointer flex-col   gap-1 py-2 px-2 experience-card  relative w-[150%]">
-      <p className="font-extrabold text-lg">{exp.title}</p>
-      <p className="text-alternate-400 text-sm">{exp.role}</p>
+    <div
+      className={`flex cursor-pointer flex-col   gap-1 py-2 px-2 experience-card  relative w-[150%] ${cardState()}`}
+      onClick={() => {
+        setActiveExp(exp)
+      }}
+    >
+      <div className={`experience-card-title ${cardState()}`}>
+        <p className="font-extrabold text-lg">{exp.title}</p>
+        <p className="text-alternate-400 text-sm">{exp.role}</p>
+      </div>
 
       <div
         className="bg-secondary-500 h-[1.5px] -bottom-2 absolute timeline "
