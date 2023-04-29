dc685547e67511ad8cc5578c1cbda40f5eeb6a79
bijuice <abdul.r.zaid@gmail.com>
2023-04-29 22:53:29 +0300

    Added Category view

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,9 @@ import Experience from "@/components/categories/Experience"
 import CATEGORY from "@/types/Category"
 import React, { useEffect, useRef, useState } from "react"
 import { motion, AnimatePresence } from "framer-motion"
+import useWindowDimensions from "@/hooks/useWindowDimensions"
+import useDeviceSize from "@/hooks/useWindowDimensions"
+import Image from "next/image"
 
 const cats = ["ABOUT", "EXPERIENCE", "SKILLS", "PROJECTS", "BLOG", "POETRY"]
 
@@ -14,6 +17,10 @@ export default function Home() {
 
   const ref = useRef<HTMLSpanElement>(null)
 
+  const [width, height] = useDeviceSize()
+
+  const [isDragging, setIsDragging] = useState(false)
+
   return (
     <main className=" h-screen px-16 overflow-hidden w-full flex items-center   select-none">
       <motion.div
@@ -21,24 +28,39 @@ export default function Home() {
         className="flex items-center gap-10 "
         drag={activeCategory === "ALL" && "x"}
         onClick={(e) => e.stopPropagation()}
-        dragConstraints={{ left: -1000, right: 0 }}
+        dragConstraints={{ right: 0, left: width > 400 ? -1000 : -2000 }}
+        onDragStart={() => setIsDragging(true)}
+        onDragEnd={() => setIsDragging(false)}
       >
         <span className="hidden" ref={ref}></span>
         {cats.map((cat, index) => (
           <CategoryCard
             key={cat}
+            isDragging={isDragging}
             index={index}
             category={cat}
             activeCategory={activeCategory}
             setCategory={setCategory}
+            width={width}
+            height={height}
           />
         ))}
       </motion.div>
+
+      {activeCategory !== "ALL" && (
+        <CategoryView
+          activeCategory={activeCategory}
+          setCategory={setCategory}
+        />
+      )}
     </main>
   )
 }
 
 type CategoryCardProps = {
+  height: number
+  width: number
+  isDragging: boolean
   index: number
   category: string
   activeCategory: string
@@ -48,10 +70,13 @@ type CategoryCardProps = {
 type CategoryState = "all" | "active" | "inactive"
 
 function CategoryCard({
+  isDragging,
   index,
   category,
   activeCategory,
   setCategory,
+  width,
+  height,
 }: CategoryCardProps) {
   function resolveCategoryState(): CategoryState {
     if (activeCategory === "ALL") {
@@ -74,12 +99,13 @@ function CategoryCard({
       zIndex: 0,
     } as any
 
+    const isPortrait = height > width
     if (resolveCategoryState() === "active") {
       properties.maxHeight = "100vh"
-      properties.maxWidth = "160vw"
+      properties.maxWidth = isPortrait ? "600vw" : "160vw"
       properties.x = 0
       properties.zIndex = 1
-      properties.left = `-${index + 1 * 20}vw`
+      properties.left = `-20vw`
     }
 
     if (resolveCategoryState() === "inactive") {
@@ -94,9 +120,14 @@ function CategoryCard({
     <motion.div
       layout
       key={category}
-      className={` relative  grow bg-teal-950 text-white text-xl  ${
+      className={` relative  grow bg-teal-950 text-white text-xl grid place-content-center  ${
         resolveCategoryState() === "all" && "cursor-pointer"
       } `}
+      onClick={() => {
+        if (!isDragging) {
+          setCategory(category)
+        }
+      }}
       animate={{
         height: animProperties().height,
         width: animProperties().width,
@@ -105,29 +136,47 @@ function CategoryCard({
         x: animProperties().x,
         left: animProperties().left,
         zIndex: animProperties().zIndex,
-        transition: {
-          duration: 0.5,
-        },
+      }}
+      transition={{
+        duration: 1,
+        type: "spring",
       }}
     >
+      {activeCategory === "ALL" && <motion.h1>{category}</motion.h1>}
+    </motion.div>
+  )
+}
+
+type CategoryViewProps = {
+  activeCategory: string
+  setCategory(category: string): void
+}
+
+function CategoryView({ activeCategory, setCategory }: CategoryViewProps) {
+  return (
+    <AnimatePresence>
       <motion.div
-        className={`absolute left-[20vw]  w-[90vw] h-screen  justify-between `}
-        layout
+        className={`fixed left-0  h-screen w-screen flex justify-between text-white p-7 z-10 `}
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+        }}
+        transition={{
+          delay: 0.3,
+        }}
       >
-        {category}
-        <motion.button
-          className="text-white "
+        <button
+          className="absolute top-10 right-10"
           onClick={(e) => {
             e.stopPropagation()
             setCategory("ALL")
           }}
-          animate={{
-            opacity: resolveCategoryState() === "active" ? 1 : 0,
-          }}
         >
           close
-        </motion.button>
+        </button>
       </motion.div>
-    </motion.div>
+    </AnimatePresence>
   )
 }
