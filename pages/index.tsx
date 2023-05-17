5272f5fab5fb3210335b95a5d8c6199e1a63f5a5
bijuice <abdul.r.zaid@gmail.com>
2023-05-17 09:28:51 +0300

    Added disclaimer

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import Close from "@/components/Icons/Close"
 import { CloseButton, NavigationButton } from "@/components/Buttons"
 import categories from "@/data/categories"
 import { ChevronRight } from "@/components/Icons/Chevrons"
+import Link from "next/link"
 
 export default function Home() {
   const [activeCategory, setActiveCategory] = useState<string>("ALL")
@@ -17,17 +18,50 @@ export default function Home() {
     setActiveCategory(category)
   }
 
+  const [showDisclaimer, setShowDisclaimer] = useState<boolean>(true)
+
   return (
     <main className=" h-screen px-16 overflow-hidden w-full flex items-center   select-none">
-      <CategoryContainer
-        activeCategory={activeCategory}
-        setCategory={setCategory}
-      />
-      {activeCategory !== "ALL" && (
-        <CategoryView
-          activeCategory={activeCategory}
-          setCategory={setCategory}
-        />
+      {showDisclaimer ? (
+        <div className="w-full h-full flex flex-col items-center justify-center text-center gap-7">
+          <h1 className="text-3xl">
+            Hello there! This site is still under construction and currently
+            looks unfinished.
+          </h1>
+          <h2 className="text-2xl">
+            You can still see my progress if you'd like or visit my old
+            portfolio if you wanna know Silicon Sorcery skills.{" "}
+          </h2>
+
+          <button
+            className="mt-7 bg-teal-800 text-white p-3 w-full max-w-[600px]"
+            onClick={() => {
+              setShowDisclaimer(false)
+            }}
+          >
+            I don't mind. Show it to me anyways {":)"}
+          </button>
+          <Link
+            href="https://super-biscuit-1456ce.netlify.app/"
+            className="p-3 text text-teal-800 font-medium"
+            target="_blank"
+          >
+            Show me the old you
+          </Link>
+        </div>
+      ) : (
+        <>
+          <CategoryContainer
+            activeCategory={activeCategory}
+            setCategory={setCategory}
+          />
+          {activeCategory !== "ALL" && (
+            <CategoryView
+              activeCategory={activeCategory}
+              setCategory={setCategory}
+            />
+          )}
+        </>
       )}
     </main>
   )
