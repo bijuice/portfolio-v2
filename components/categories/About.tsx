53da2367d64a59d7b7cc66442bcde4bde2af8909
bijuice <abdul.r.zaid@gmail.com>
2023-05-21 11:33:48 +0300

    Added more detail in about component

--- components/categories/About.tsx
+++ components/categories/About.tsx
@@ -1,3 +1,10 @@
+import { Heading, SubHeading } from "../Typography"
+
 export default function About() {
-  return <></>
+  return (
+    <section className="flex flex-col gap-5 justify-center items-between px-20 h-full w-full">
+      <Heading text="Abdul Rahman Rehmtulla" />
+      <SubHeading text="fullstack, cat dad, poet." />
+    </section>
+  )
 }
