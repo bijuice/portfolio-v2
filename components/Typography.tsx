435220078b0b18dc382a7394b6db8e3b9603e424
bijuice <abdul.r.zaid@gmail.com>
2023-06-30 21:07:42 +0300

    Added text animation

--- components/Typography.tsx
+++ components/Typography.tsx
@@ -48,7 +48,7 @@ export function PageHeading({ text }: TypographyProps) {
 export function PageSubHeading({ text }: TypographyProps) {
   return (
     <div
-      className={`px-4 overflow-hidden text-xl 2xl:text-4xl text-center font-['Circular'] italic `}
+      className={`px-4 overflow-hidden text-xl 2xl:text-4xl text-center  italic `}
     >
       <motion.h2
         initial={{
@@ -77,7 +77,7 @@ export function SectionHeading({
 }) {
   return (
     <h2
-      className={`avant-garde  bg-clip-text  text-6xl 2xl:text-8xl leading-relaxed  text-black uppercase`}
+      className={`avant-garde  bg-clip-text  text-[5em] leading-relaxed  text-black uppercase`}
     >
       {children}
     </h2>
@@ -85,7 +85,53 @@ export function SectionHeading({
 }
 
 export function TextEmphasis({ children }: { children: React.ReactNode }) {
-  return <span className="font-bold text-[1.5em] uppercase ">{children}</span>;
+  const animationDuration = 0.6;
+
+  return (
+    <motion.span
+      className="font-bold text-[1.2em] uppercase px-4 mx-1"
+      style={{
+        background: `linear-gradient(to left, white, white 50%, black 50%, black)`,
+        backgroundSize: "200.3% 100%",
+      }}
+      initial={{
+        backgroundPosition: "100% 0%",
+      }}
+      whileInView={{
+        backgroundPosition: "0% 0%",
+      }}
+      transition={{
+        duration: animationDuration,
+      }}
+      viewport={{
+        amount: "all",
+      }}
+    >
+      <motion.span
+        className="break-words"
+        style={{
+          background: `linear-gradient(to left, black, black 50%, white 50%, white)`,
+          backgroundSize: "200.3% 100%",
+          WebkitBackgroundClip: "text",
+          WebkitTextFillColor: "transparent",
+        }}
+        initial={{
+          backgroundPosition: "100% 0%",
+        }}
+        whileInView={{
+          backgroundPosition: "0% 0%",
+        }}
+        transition={{
+          duration: animationDuration,
+        }}
+        viewport={{
+          amount: "all",
+        }}
+      >
+        {children}
+      </motion.span>
+    </motion.span>
+  );
 }
 
 export function Skill({ children }: { children: React.ReactNode }) {
