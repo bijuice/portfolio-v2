7281896929c29ef5e677205d92e7b2f217bda5f9
bijuice <abdul.r.zaid@gmail.com>
2023-07-05 18:33:50 +0300

    Added favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,35 @@
-import Layout from "@/components/layout/Layout"
-import "@/styles/globals.css"
-import type { AppProps } from "next/app"
+import Layout from "@/components/layout/Layout";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest"></link>
+      </Head>
       <style jsx global>{``}</style>
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </>
-  )
+  );
 }
