7a460118200b4bd3b14c9d4d531096b3b48ff90d
bijuice <abdul.r.zaid@gmail.com>
2023-08-06 19:55:20 +0300

    Added 3D effect to project title

--- components/categories/ProjectsPage.tsx
+++ components/categories/ProjectsPage.tsx
@@ -3,18 +3,79 @@ import { Project } from "@/types/Project";
 import projects from "@/data/projects";
 import Link from "next/link";
 import Image from "next/image";
+import { motion } from "framer-motion";
+import { useRef, useState } from "react";
+import getRelativeCoordinates from "@/utilities/getRelativeCoordinates";
+
+function ShadowHeading({
+  coords,
+  styles,
+  index,
+}: {
+  coords: {
+    x: number;
+    y: number;
+  };
+  styles: string;
+  index: number;
+}) {
+  const k = 0.003 * index;
+
+  const x = coords.x > 1000 ? 5 + 1000 * k : 5 + coords.x * k;
+  const y = coords.y < 500 ? 5 + coords.y * k : 5 + 500 * k;
+
+  return (
+    <motion.div
+      className={`absolute ${styles} nth-child`}
+      animate={{
+        x: x,
+        y: y,
+      }}
+      transition={{
+        type: "tween",
+        duration: 0.5,
+      }}
+    >
+      Projects
+    </motion.div>
+  );
+}
 
 export default function ProjectsPage() {
+  const pageRef = useRef<any>();
+
+  const handleMouseMove = (e: any) => {
+    const { x, y } = getRelativeCoordinates(e, pageRef.current);
+
+    setCoords({ x, y });
+  };
+
+  const [coords, setCoords] = useState({
+    x: 0,
+    y: 0,
+  });
+
   return (
-    <div className="category-view   max-h-screen w-screen overflow-x-hidden overflow-y-auto ">
-      <section className=" flex flex-col items-center  gap-5  h-screen relative p-24  ">
-        <h1 className="text-7xl uppercase avant-garde">Projects</h1>
+    <div
+      className="category-view   max-h-screen w-screen overflow-x-hidden overflow-y-auto "
+      ref={pageRef}
+      onMouseMove={handleMouseMove}
+    >
+      <section className=" flex flex-col items-center  gap-5  h-screen relative p-24  mt-16">
+        <div className="text-7xl uppercase avant-garde min-h-16">
+          <ShadowHeading coords={coords} styles="text-red-600 " index={0} />
+          <ShadowHeading coords={coords} styles="text-purple-500" index={1} />
+          <ShadowHeading coords={coords} styles="text-green-500" index={2} />
+          <ShadowHeading coords={coords} styles="text-black" index={3} />
+
+          <h1 className="text-transparent">Projects</h1>
+        </div>
 
         <h2 className="mt-7 text-xl">A selection of my finest projects.</h2>
 
         <h3> Made from organic, gluten free Stack Overflow code snippets.</h3>
 
-        <div className="w-full flex flex-col  pt-10 pb-16 px-10 md:px-20    gap-y-24 items-center  ">
+        <div className="w-full flex flex-col  py-20 px-10 md:px-20    gap-y-24 items-center  ">
           {projects.map((project) => (
             <ProjectCard key={project.name} proj={project} />
           ))}
